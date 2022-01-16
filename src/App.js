7c9b001233c7d674f0102a1c46325099134011cd
pmuhire2002 <76108262+pmuhire@users.noreply.github.com>
2022-01-16 22:11:08 +0200

    Added React Router Dom

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
-import React from 'react';
-import './App.css';
-import HomePage from './pages/homepage/homepage.component';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import HomePage from "./pages/homepage/homepage.component";
+
+const HatsPage = () => {
+  return (
+    <div>
+      <h1>Hats Page</h1>
+    </div>
+  );
+};
 
 class App extends React.Component {
-  render(){
+  render() {
     return (
-      <div >
-         <HomePage/>
+      <div>
+        <Routes>
+          <Route exact path="/" element={<HomePage></HomePage>} />
+          <Route path="/hats" element={<HatsPage></HatsPage>} />
+        </Routes>
       </div>
     );
   }
-  
 }
 
 export default App;
